96315bf70e9b39f35a31505e7cac6e4a95dd9454
Mario Windsor <mario@lazaro.in>
2018-12-18 11:21:53 +0530

    Stage Text Content

--- index.php
+++ index.php
@@ -45,34 +45,163 @@
 
 <div id="page-wrapper" data-page="<?php echo $viewName ?>"><!-- Page Wrapper -->
 
-	<!-- Header Section -->
-	<section class="header-section">
-		<div class="container">
-			<div class="header row">
-				<div class="columns small-3">
-					<a class="logo" href="/">
-						<img src="media/logo.svg<?php echo $ver ?>">
-					</a>
-				</div>
-				<div class="text-right columns small-9">
-					<div class="navigation inline">
-						<a class="button js_nav_button <?php echo ( $viewName == "pageone" ? "active" : "" ) ?>" data-page-id="pageone" href="/pageone">page-1</a>
-						<a class="button js_nav_button <?php echo ( $viewName == "pagetwo" ? "active" : "" ) ?>" data-page-id="pagetwo" href="/pagetwo">page-2</a>
-						<a class="button js_nav_button <?php echo ( $viewName == "page-bg-video" ? "active" : "" ) ?>" data-page-id="page-bg-video" href="/page-bg-video">Background video</a>
-						<a class="button js_nav_button <?php echo ( $viewName == "contact" ? "active" : "" ) ?>" data-page-id="contact" href="/contact">contact</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> <!-- END : Header Section -->
+	<pre>
+	Apartment
+
+	Rich, Comfy and Better Engineered.
+
+	Klassik Landmark.
+	You don’t have to be a big builder to do things better.
+
+	The only ‘all 3BHK’ apartment development in the Sarjapur Road vicinity.
+
+	Island 3BHK
+	2035sft of Privacy
+
+	No Common Walls.
+	Only 3 Flats Per Floor.
+	Bright, Airy Kitchen & Utility.
+	Larger Balconies
+	20% More Room Volume
+	9'6" Clear Ceiling-Height.
+	Aerodynamic Tower Boosts Ventilation.
+	Tall 8 Feet Entry Door 
+	No Visible Beams
+
+	Private and Detached
+	Your privacy is a necessity.
+
+	No common walls, only 3 flats per floor
+	
+	The 2000sft ‘Island 3BHK’ does not share any common walls with any other apartment. It is the only apartment in the vicinity that opens to the East and the West. Every lift lobby only serves 3 apartments per floor. Each lift lobby has 2 lifts, one 13 person lift and one 6 person lift.
+
+	ISLAND /ˈ ʌɪlənd/ 
+	Noun - a thing regarded as resembling an island, especially in being isolated or detached.
+
+	Enquire Now
+	Name
+	Email
+	Phone
+
+	Aerodynamics to improve ventilation
+
+	A direct benefit of the 'wing-shaped' towers, better airflow around the buildings. Placing 3 balconies on this aerodynamic curve significantly boosts the flow of air into all rooms. Klassik Landmark has more balconies per apartment than any other project in the vicinity.
 
 
-	<!-- Page Content -->
-	<div id="page-content">
+	Large openings, more balconies, more sunlight 
+	
+	The sliding doors that lead to a balcony are a much larger openings, compared to a regular window, they let in more sunlight and fresh air.
 
-		<?php require $viewPath; ?>
 
-	</div> <!-- END : Page Content -->
+	Bright airy kitchen and utility 
+	
+	The Kitchen is positioned closer to the building line, it opens out into a fully ventilated utility. This is a best in class feature.
+
+
+	20% higher room volume 
+
+	The ‘flat-slab’ engineering upgrade allows for larger room sizes, an improved ceiling height of 9’6” and no visible beams, because there are no beams. All internal walls are not load bearing. 
+
+
+	Takeover a Fresh Apartment Engineering Upgrade 
+
+	We changed one engineering process to make your new apartment feel new.  The Doors, Sanitary Fixtures and Kitchen Counter are installed just before you move in. They are not soiled by construction workers. The apartment truly feels brand new. 
+
+	Top of the line building materials from Schindler, Grohe, Legrand, Grundfos, Pyramid, Saint Gobain & Nippon paint.
+
+
+	Building
+
+	Responsible engineering adjusts for local wind and sunlight conditions
+
+	Constructing a curved aerodynamic building is close to 10% more expensive. The result is benefits that are worth it, considering the long lifespan of a civil construction. 
+
+
+	Aligned to Bangalore’s natural wind direction  Design Upgrade 
+
+	Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proiden.
+
+
+	‘Aircraft Wing Section’ tower design Engineering Upgrade 
+
+	We changed one engineering process to make your new apartment feel new.  The Doors, Sanitary Fixtures and Kitchen Counter are installed just before you move in. They are not soiled by construction workers. The apartment truly feels brand new. 
+
+
+	8.5 acres of open, traffic-free podium Design Upgrade 
+
+	We optimised the building footprint down to just 20% of the total of 11 acres. The remaining 80% is a combination of green space & amenities for residents. We also improved on the industry norm by completely removing surface parking along the boundary wall. There is no vehicular traffic at the podium level. So that’s less noise, less dust, and a much needed child-friendly zone.
+
+
+	Scientifically proven higher air quality
+	Periodically conducted, scientific measurements of air quality at the project.
+
+	Optimised Building Footprint
+	80% open space.
+	Wide views from curved balconies. 
+	Balconies Either Face East or West. 
+	Aligned With 80% Of Bangalore’s Wind. 
+	Privacy For Ground Floor Flats. 
+
+	Pollution Board Air Quality Certificate:
+	Download
+
+
+	Location
+
+	10 min drive to Wipro Head Office
+	6 km from Ecospace on the ORR 
+	9 km before Electronic City. 
+	10 min to Hosa Road Metro Station. 
+
+	The only ‘all 3BHK’ apartment in the Sarjapur Road vicinity.
+
+	Optimal work commutes Engineering Upgrade
+
+	Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proiden.
+
+
+	Footer
+
+	Quick Links
+
+	Island 3BHK
+
+	Floorplan
+	Masterplan
+	Amenities
+	Specifications
+	Location
+
+	Plush 3BHK
+
+	Floorplan
+	Masterplan
+	Amenities
+	Specifications
+	Location
+
+	Elevate 3BHK
+
+	Floorplan
+	Masterplan
+	Amenities
+	Specifications
+	Location
+
+	Project Office
+
+	Sy No.34/9 - 34/13, Junnasandra, Varthur, 
+	Near Amritha College Of Engineering,
+	Bangalore - 560 035.
+
+	+91–96633–96887
+
+	Seyonii Badge
+
+
+	DISCLAIMER : The content mentioned on this website is for general information purposes only. While enough care is taken by Klassik Enterprises Pvt Ltd to ensure that information on the website is up to date, accurate and correct, readers are requested to make their independent enquiry before relying upon the same. In no event will Klassik Enterprises offer any warranty on the information made available, or be liable for any loss or damage including without limitation, indirect or consequential loss or damage in connection with, the use of information in this website. By using or accessing the website, you agree with the Disclaimer without any qualification or limitation. Design, specifications, floor plans, brochures, 360 degree views and interactive plans etc., are subject to change without prior notice. Computer generated images; walkthroughs and render images are the artist’s impression and are an indicative of the actual designs. The contents of this website are meant to provide information to the readers of this website about ourselves including our various projects, etc. They are only for general information and are subject to change. By no stretch of imagination, the information on the website shall be construed as an advertisement and/or invitation or offer for sale. To find out more about projects/developments, please call 9663-396-887 or visit our sales office during working hours and get in touch with authorized Klassik Enterprises sales representative.
+
+	</pre>
 
 
 	<!-- Lazaro Signature -->
